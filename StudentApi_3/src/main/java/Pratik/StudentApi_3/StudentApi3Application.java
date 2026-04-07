abad6fa1e370a7da141f6f95ce1f3d7452a0d47f
Pratik Patil <pratikpatil2069@gmail.com>
2026-04-07 16:06:03 +0530

    explained the terminologis that are used in this project

--- StudentApi_3/src/main/java/Pratik/StudentApi_3/StudentApi3Application.java
+++ StudentApi_3/src/main/java/Pratik/StudentApi_3/StudentApi3Application.java
@@ -11,3 +11,10 @@ public class StudentApi3Application {
 	}
 
 }
+
+/* 1.@Document(Collection="studentSchemas"):-this annotation is used to specify the collections in database for store documents.
+   2.@Autowire:- this ask the spring to give the object of particular class from ioc container.
+   3.@PathVariable:- this annotation is used to get the variables that shared from request.
+   4.@RequestBody:- used to get the request body that send from front end through url.
+   5.@Services:-this class contains main logic and spring creates automatically object of that class.
+*/
